e1aab1abde908f9d84662fbd0fea529ecbf1b812
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-03-08 13:08:33 -0500

    fix

---
+++ web3/header2/script.js
@@ -0,0 +1,27 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu() {
+    const nav = document.getElementById('nav');
+
+
+    nav.classList.toggle('active');
+
+}
+
+
+(function() {
+    setInterval(function() {
+        var canbio = document.getElementById("logo");
+        primera = ['teal', "white"];
+
+        canbio.style.color = primera[Math.floor(Math.random() * primera.length)];
+    }, 1000);
+}())
+(function() {
+    setInterval(function() {
+        var canbio = document.getElementById("verde");
+        primera = ['green', "white"];
+
+        canbio.style.color = primera[Math.floor(Math.random() * primera.length)];
+    }, 1000);
+}())
