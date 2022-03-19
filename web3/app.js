7dc3c3fc6236d4790159c358e9518393f5ff1c6e
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-03-19 06:47:00 -0400

    fix

--- web3/app.js
+++ web3/app.js
@@ -4,3 +4,22 @@ function toggleMenu() {
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
+
+
+
+(function() {
+    setInterval(function() {
+        var canbio = document.getElementById("logo");
+        primera = ['teal', "white"];
+
+        canbio.style.color = primera[Math.floor(Math.random() * primera.length)];
+    }, 1000);
+}())
+(function() {
+    setInterval(function() {
+        var canbio = document.getElementById("verde");
+        primera = ['green', "white"];
+
+        canbio.style.color = primera[Math.floor(Math.random() * primera.length)];
+    }, 1000);
+}())
