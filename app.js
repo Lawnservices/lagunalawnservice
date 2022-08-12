a0eb1c0f7294752d0151e21cbeaf58fe50a7e96e
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-08-11 21:43:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector('.header .nav-bar nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar nav-list ul');
-const mobile_menu = document.querySelector('.header .nav-bar nav-list ul  li a');
+const mobile_item = document.querySelector('.header .nav-bar nav-list ul  li a');
 const header = document.querySelector('.header.container');
 
 
@@ -18,8 +18,8 @@ document.addEventListener('scroll', () => {
     }
 
 });
-menu_item.array.forEach(item => {
-    item.addEventListener('click', () => {
+menu_item.forEach(item => {
+    item.addEventListener('click',() => {
         hamburger.classList.toggle('active');
         mobile_menu.classList.toggle('active');
     });
