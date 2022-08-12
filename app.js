0095be847a3a6f8b29bbaf1bec61b7f1cb50393b
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-08-11 21:02:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul ');
-
-
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
 
@@ -9,9 +8,9 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
-document.addEventListener('scroll', () => {
+document.addEventListener('scroll',()=> {
     var scroll_position = window.scrollY;
-    if (scroll_position > 200) {
+    if (scroll_position > 200){
         header.style.backgroundColor = '#29323c';
     } else {
         header.style.backgroundColor = 'transparent';
@@ -19,7 +18,7 @@ document.addEventListener('scroll', () => {
     }
 
 });
-menu_item.array.forEach(item => {
+menu_item.forEach((item => {
     item.addEventListener('click', () => {
         hamburger.classList.toggle('active');
         mobile_menu.classList.toggle('active');
