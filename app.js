ab1b75153a75d9d825e6bc53869b3d42a789fc3c
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-08-11 21:52:20 -0400

    fix

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
  const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
  const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
@@ -15,16 +14,9 @@
          header.style.backgroundColor = '#29323c';
      } else {
          header.style.backgroundColor = 'transparent';
-=======
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
->>>>>>> 17d3962614e12cc6a60d4af8faf8d9a1f334f3af
 
      }
 
-<<<<<<< HEAD
  });
  menu_item.forEach(item => {
      item.addEventListener('click', () => {
@@ -33,26 +25,3 @@ const header = document.querySelector('.header.container');
      });
 
  });
-=======
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
-});
-document.addEventListener('scroll',()=> {
-    var scroll_position = window.scrollY;
-    if (scroll_position > 200){
-        header.style.backgroundColor = '#29323c';
-    } else {
-        header.style.backgroundColor = 'transparent';
-
-    }
-
-});
-menu_item.forEach((item => {
-    item.addEventListener('click', () => {
-        hamburger.classList.toggle('active');
-        mobile_menu.classList.toggle('active');
-    });
-
-});
->>>>>>> 17d3962614e12cc6a60d4af8faf8d9a1f334f3af
