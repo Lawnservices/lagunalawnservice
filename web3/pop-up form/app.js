fbb6057747aba8c22a8aa82c862af7274641ef05
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-09-05 19:31:12 -0400

    fix

---
+++ web3/pop-up form/app.js
@@ -0,0 +1,8 @@
+ function laguna() {
+     document.getElementById('colores').style.backgroundColor = '#154256'
+     document.getElementById('nicole').style.color = 'green'
+
+ };
+
+ document.getElementById('colores').addEventListener('click', laguna);
+ document.getElementById('nicole').addEventListener('click', laguna);
