e04d2487c3856ad6407ca269a0697b5d9f318542
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-09-23 17:38:08 -0400

    fix

--- web2/PHOTOS/app.js
+++ web2/PHOTOS/app.js
@@ -4,7 +4,10 @@ document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
     if (scroll_position > 250) {
         header.style.backgroundColor = 'green';
+        header.style.borderBottom = '3px solid #154256'
     } else {
         header.style.backgroundColor = ' #154256';
+        header.style.borderBottom = '3px solid green '
+
     }
 });
