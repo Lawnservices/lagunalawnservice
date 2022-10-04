fc67545737682a54bdc0681b910ec67bd2acf864
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-10-04 19:38:42 -0400

    fix

--- web2/app.js
+++ web2/app.js
@@ -1,41 +1,11 @@
 const laguna = document.getElementById('colores');
 
 document.addEventListener('scroll', () => {
-    var scroll_position = (window.scrollY);
-    if (scroll_position > 250) {
-        colores.style.backgroundColor = '#29323c';
+    var scroll_position = window.scrollY;
+    if (scroll_position > 160) {
+        colores.style.backgroundColor = '#04AA6D'
     } else {
         colores.style.backgroundColor = '#60a5a5'
     }
+
 });
-
-
-window.sr = ScrollReveal();
-
-
-ScrollReveal().reveal('.navegacion1', { delay: 3000, });
-sr.reveal('.caja1', {
-    duration: 4000,
-    origin: 'bottom',
-    distance: '200px'
-});
-
-(function() {
-    setInterval(function() {
-        var canbio = document.getElementById("verde_v"),
-            colores = ["blue", "teal"];
-
-        canbio.style.color = colores[Math.floor(Math.random() * colores.length)];
-    }, 2000);
-
-}())
-
-(function() {
-    setInterval(function() {
-        var segunda = document.getElementById("pregunte"),
-            primera = ["teal", "green"];
-
-        segunda.style.color = primera[Math.floor(Math.random() * primera.length)];
-    }, 2000);
-
-}());
