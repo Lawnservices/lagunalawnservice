9b4290da09a9c433a6ec46d4bdb44e27c4440dc0
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-10-19 12:33:13 -0400

    fix

---
+++ web2/header.js
@@ -0,0 +1,10 @@
+const laguna = document.getElementById('mnb');
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 160) {
+        mnb.style.backgroundColor = '#04aa6d';
+    } else {
+        mnb.style.backgroundColor = '#60a5a5';
+    }
+})
