83de0fc54329c3ecc91825f330353ddc6be47af9
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-10-24 11:26:58 -0400

    fix

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
     if (scroll_position > 250) {
         header.style.backgroundColor = '#29323c';
+
     } else {
         header.style.backgroundColor = 'transparent';
 
