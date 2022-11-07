eef366dbde4625059b7baf9f9a66e33a0b561fd9
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-11-07 14:56:54 -0500

    fix

--- shop/shop.js
+++ shop/shop.js
@@ -1,4 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const imgbox = document.querySelectorAll('.material');
-    M.Materialbox.init(imgbox);
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     const imgbox = document.querySelectorAll('.material');
+//     M.Materialbox.init(imgbox);
+// });
+
+const lasName = (id, Name) => ({ id: id, Name: Name });
+console.log(lasName(10, "cmon witcha say hello"));
+
+const multiply = (m, z) => m * z;
+console.log(multiply(10, 20));
+
+const hello = () => console.log("hello world");
+hello();
+
+const car = (id, Color) => ({ id: id, Color: Color });
+console.log(car(22, "red"));
+
+const Saludar = (nombre) => ({ nombre: nombre });
+
+console.log(Saludar("miguel Gracias por benir"));
+
+
+let laname = "miguel laguna";
+console.log(laname);
