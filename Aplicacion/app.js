fad1fc14c4ddd10110e0bb512cc9ae20ef877bca
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-11-08 13:53:51 -0500

    fix

---
+++ Aplicacion/app.js
@@ -0,0 +1,106 @@
+ document.getElementById('formTask').addEventListener('submit', saveTask);
+
+ function saveTask(e) {
+
+     let title = document.getElementById('title').value;
+     let description = document.getElementById('description').value;
+     let fecha = document.getElementById('fecha').value;
+
+     const task = {
+         title,
+         description,
+         fecha
+     };
+
+     if (localStorage.getItem('tasks') == null) {
+         let tasks = [];
+         tasks.push(task);
+         localStorage.setItem('tasks', JSON.stringify(tasks));
+     } else {
+         let tasks = JSON.parse(localStorage.getItem('tasks'));
+         tasks.push(task);
+         localStorage.setItem('tasks', JSON.stringify(tasks));
+     }
+
+     getTasks();
+
+     document.getElementById('formTask').reset();
+     e.preventDefault();
+ }
+
+ function getTasks() {
+     let tasks = JSON.parse(localStorage.getItem('tasks'));
+     let tasksView = document.getElementById('tasks');
+
+     tasksView.innerHTML = '';
+
+     for (let i = 0; i < tasks.length; i++) {
+         let title = tasks[i].title;
+         let description = tasks[i].description;
+         let fecha = tasks[i].fecha;
+
+         tasksView.innerHTML += `<div class="card mb-3">
+        <div class="card-body">
+        <p>${title} - ${description} - ${fecha}</p>
+        <a class="btn btn-danger" onclick="deleteTask('${title}')">
+        Eliminar
+        </a>
+        </div>
+        <div>`
+     }
+ }
+
+ function deleteTask(title) {
+     let tasks = JSON.parse(localStorage.getItem('tasks'));
+     for (let i = 0; i < tasks.length; i++) {
+         if (tasks[i].title == title) {
+             tasks.splice(i, 1);
+         }
+     }
+     localStorage.setItem('tasks', JSON.stringify(tasks));
+     getTasks();
+ }
+ getTasks();
+
+
+
+ // todo es galleta
+ function setCookie(cname, cvalue, exdays) {
+     const d = new Date();
+     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+     let expires = "expires=" + d.toUTCString();
+     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+ }
+
+ function getCookie(cname) {
+     let name = cname + "=";
+     let decodedCookie = decodeURIComponent(document.cookie);
+     let ca = decodedCookie.split(';');
+     for (let i = 0; i < ca.length; i++) {
+         let c = ca[i];
+         while (c.charAt(0) == ' ') {
+             c = c.substring(1);
+         }
+         if (c.indexOf(name) == 0) {
+             return c.substring(name.length, c.length);
+         }
+     }
+     return "";
+ }
+
+ function checkCookie() {
+     let user = getCookie("username");
+     if (user != "") {
+         alert("Bienvenida de Nuevo " + user);
+     } else {
+         user = prompt("Ponga su Nombre:", "");
+         if (user != "" && user != null) {
+             setCookie("username", user, 30);
+         }
+     }
+ }
+
+ // localStorage.setItem('tasks', JSON.stringify('task'));
+ // localStorage.getItem('tasks');
+
+ // document.getElementById('formTask').reset();
