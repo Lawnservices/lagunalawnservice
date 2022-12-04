12e052aecdcb8dc806d06faf4144c0cc5a9ce7dc
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-12-03 20:06:39 -0500

    fix

--- query.js
+++ query.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
-    $("ul li a:first").addClass('active');
 
-    $("ul li a").click(function() {
-        $("ul li a").removeClass('active');
-        $(this).addClass('active');
-    })
+
 
 
 });
