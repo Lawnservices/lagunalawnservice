15b834ec236b028fc123ed9bae7f3265bccc2d77
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-12-04 15:48:23 -0500

    fix-web

--- photos.js
+++ photos.js
@@ -13,30 +13,14 @@ document.addEventListener('scroll', () => {
 });
 
 
-const fotos = document.getElementById('mnb');
-document.addEventListener('scroll', () => {
-    var scroll_position = window.scrollY;
-    if (scroll_position > 250) {
-        mnb.style.width = '80%';
-        mnb.style.transition = '2s '
-
-    } else {
-        mnb.style.width = '100%';
-
-    }
-});
 
 const mexi = document.getElementById('colores');
 document.addEventListener('scroll', () => {
-        var scroll_position = window.scrollY;
-        if (scroll_position > 250) {
-            colores.style.backgroundColor = '#154256';
-        } else {
-            colores.style.backgroundColor = '#04aa6d';
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        colores.style.backgroundColor = '#154256';
+    } else {
+        colores.style.backgroundColor = '#04aa6d';
 
-        }
-    })
-    // document.getElementById('mnb').addEventListener('click', function() {
-    //     this.style.marginLeft = '20%';
-    //     this.style.width = '60%';
-    // })
+    }
+})
