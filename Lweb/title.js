ed21f6cae1d741424a2699883873270ee92a436f
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-12-07 19:06:24 -0500

    fix-web

--- Lweb/title.js
+++ Lweb/title.js
@@ -1,10 +1,14 @@
 const laguna = document.getElementById('foto');
 
+
 document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
-    if (scroll_position > 100) {
+    if (scroll_position > 200) {
         foto.style.backgroundColor = "#154";
+
     } else {
         foto.style.backgroundColor = "#0366b6";
+        foto.style.height = "220px";
+
     }
 });
