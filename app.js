c8a2f5abe529d5513ab79509c1f49b345be3f420
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-01-23 16:32:19 -0500

    fix

--- app.js
+++ app.js
@@ -39,10 +39,10 @@ function myFunction() {
 function laguna() {
     document.getElementById('blanco').style.color = 'blue';
     document.getElementById('blancoo').style.color = '#fff';
-    window.confirm('subscribe to our channel the YouTube');
-    if (window.confirm("gracias suscribete")) {
-        window.open("https://lagunalawnservice.com/", "Thanks for Visiting!");
-    }
+    // window.confirm('subscribe to our channel the YouTube');
+    // if (window.confirm("gracias suscribete")) {
+    //     window.open("https://lagunalawnservice.com/", "Thanks for Visiting!");
+    // }
 
 
 }
