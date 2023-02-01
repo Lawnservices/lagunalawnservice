ebc4aa5fd024d0729528b84826e28360b5648ea4
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-02-01 08:26:16 -0500

    fix_web

--- photos.js
+++ photos.js
@@ -14,3 +14,10 @@ document.addEventListener('scroll', () => {
 function myFunction() {
     alert('This document is now being printed');
 }
+
+function laguna() {
+    document.getElementById('nombre').style.color = '#0366d6';
+    document.getElementById('calor').style.color = '#04aa6b';
+    document.getElementById('calor').innerHTML = 'Call now to make an estimate <br>240-883-8479';
+    document.getElementById('zxcv').innerHTML = 'Subscribete to our Youtube channel';
+}
