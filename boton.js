6d54b87a3ee663b3186eb194243e5ebabb210175
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-02-17 21:42:16 -0500

    fix

--- boton.js
+++ boton.js
@@ -7,6 +7,10 @@ setTimeout(function() {
 function setColor() {
     var pagina = document.body;
     pagina.style.backgroundColor = pagina.style.backgroundColor == "#fff" ? "green" : "#B5D8F0";
+    var pares = document.querySelector('.cta');
+    pares.style.backgroundColor = pares.style.backgroundColor = "#f6f6f6";
+    pares.style.color = pares.style.color = "#04aab6";
+
 
 }
 
