f1e21d73b4b0a6e2daa0df17a1c7c023f81a84e0
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-02-20 18:00:27 -0500

    fit

--- prices.js
+++ prices.js
@@ -1,12 +1,23 @@
-document.getElementById('print').addEventListener('click', function() {
-    print.style = print();
+ document.getElementById('print').addEventListener('click', function() {
+     print.style = print();
+     if (print) {
+         this.style.backgroundColor = '#04aab6';
+         this.style.marginTop = '160px';
+
+     }
 
 
+ });
+ document.getElementById("mensaje").innerHTML = 'Thank you please subscribe to our youtube channel.';
 
-});
-document.getElementById("mensaje").innerHTML = 'Thank you please subscribe to our youtube channel.';
+ function Laguna() {
+     alert('This document is now being printed');
+     if (
+         mensaje.innerHTML = 'Call us you will have the best prices'
 
-function Laguna() {
-    alert('This document is now being printed');
+     )
+         if (
+             mensaje.style.color = '#0366b6'
+         );
 
-}
+ };
