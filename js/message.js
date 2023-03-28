9e42f8c4c7b42513ac6c5564e24abf130e2dbfe3
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-03-28 15:36:23 -0400

    fix-web

--- js/message.js
+++ js/message.js
@@ -8,7 +8,7 @@ showMessage("Call For Services:\n");
 function pagos(precio) {
     document.getElementById('precio').textContent = precio;
     document.getElementById('precio').style.fontSize = '2.1rem';
-    document.getElementById('precio').style.color = '#0366d6';
+    document.getElementById('precio').style.color = '#299684';
 }
 
-pagos('Shrub pruning if you hire our services');
+pagos('Time to replace the old mulch call us for the special');
--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,6 @@
 ScrollReveal({ duration: 1000 });
 
-ScrollReveal().reveal('.lanw', { delay: 3000 });
-ScrollReveal().reveal('.icon', { delay: 4000 });
-ScrollReveal().reveal('.btn', { delay: 3000 });
-ScrollReveal().reveal('.verde', { delay: 5000 });
+ScrollReveal().reveal('.lanw', { delay: 1000 });
+ScrollReveal().reveal('.icon', { delay: 1000 });
+ScrollReveal().reveal('.btn', { delay: 1000 });
+ScrollReveal().reveal('.verde', { delay: 1000 });
