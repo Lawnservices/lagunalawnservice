b3d6f736a7196ef32e5b36db93b3c5ca11dc4eb0
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-06-01 17:31:58 -0400

    fix

--- js/message.js
+++ js/message.js
@@ -15,12 +15,12 @@ pagos('Time to replace the old mulch call us for the special');
 
 
 // 0000
-if('geolocation' in navigator){
+// if('geolocation' in navigator){
 
-} else{}
-navigator.geolocation.getCurrentPosition(function(position){
-var latitude = position.latitude;
-var longitude = position.coords.longitude;
-}), function(error){
+// } else{}
+// navigator.geolocation.getCurrentPosition(function(position){
+// var latitude = position.latitude;
+// var longitude = position.coords.longitude;
+// }), function(error){
 
-};
+// };
