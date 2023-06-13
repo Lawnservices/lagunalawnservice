13cf807df8ece0ee648e28a723a9972593a2eec7
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-06-13 16:25:05 -0400

    fix

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
-
+const ver = document.getElementById('verde');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
@@ -21,19 +21,32 @@ menu_item.forEach((item) => {
 document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
     if (scroll_position > 350) {
-        header.style.backgroundColor = '#04aa6d';
+        header.style.backgroundColor = '#0366b6';
         header.style.borderTop = '6px solid #34495e';
         header.style.height = '80px';
+          
+        
         if (scroll) {
-            header.style.height = '100px';
-
-
-        }
+            header.style.height = '80px';
+       }
     } else {
         header.style.backgroundColor = '#34495e';
         header.style.borderTop = '6px solid #04aa6d';
         header.style.height = '80px';
     }
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 600){
+
+   header.style.borderBottom= '4px solid #ed8080';
+   verde.style.display = 'none';
+    } else{
+          header.style.borderBottom= 'none';
+          verde.style.display = 'none';
+    }
+
 
 });
 
