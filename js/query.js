3fa157a25532041f7c8a1ecd7a8e39f7b7c683b3
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-06-15 16:06:01 -0400

    fix

--- js/query.js
+++ js/query.js
@@ -1,12 +1,13 @@
 var miParte = $('#vermas');
- 
+var mas = $('.lugares');
 $(document).ready(function() {
     $('.btn').click(function() {
         $('.orario').slideToggle("slow");
         $('#mass').slideToggle("slow");
        miParte.css('color','#036');
        miParte.css('fontFamily','verdana');
-          
+       $('.lugares').text('FREE ESTIMATE.');
+        mas.css('color','blue');
     });
 
 
