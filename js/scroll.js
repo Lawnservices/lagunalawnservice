6b010f9031b33b47fec77ddb009a14d569b8d3a2
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-09-28 17:07:30 -0400

    fix

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,4 @@
 ScrollReveal({ duration: 1000 });
 
 ScrollReveal().reveal('.lanw', { delay: 1000 });
-ScrollReveal().reveal('.icon', { delay: 1000 });
-ScrollReveal().reveal('.btn', { delay: 1000 });
+ 
--- reveal.js
+++
@@ -1,4 +0,0 @@
-ScrollReveal().reveal('#uno', {duration:9000});
-ScrollReveal().reveal('.mensaje', { duration: 4000 });
-ScrollReveal().reveal('#mass', { duration: 6000 }); 
- 
