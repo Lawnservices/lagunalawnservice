0fa999507afa44892c445c1a49c939acf6b2a36d
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-10-30 18:41:58 -0400

    fix

---
+++ place.js
@@ -0,0 +1,9 @@
+const myPromise = new Promise(function(myResolve, myReject) {
+  setTimeout(function(){ myResolve("You want an excellent garden, you are in the right place"); }, 6000);
+});
+
+myPromise.then(function(value) {
+  document.getElementById("place").innerHTML = value;
+  document.getElementById("place").style.backgroundColor="#1548";
+});
+
