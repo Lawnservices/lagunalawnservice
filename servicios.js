8eb9a53b533ceda978284b82bf495ff63a7c1bea
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-11-20 21:11:46 -0500

    fix

---
+++ servicios.js
@@ -0,0 +1,7 @@
+document.getElementById('mostrarVideo').addEventListener('click', function() {
+  // Obtén el contenedor del video
+  var videoContainer = document.getElementById('videoContainer');
+
+  // Muestra el contenedor del video al hacer clic
+  videoContainer.style.display = 'block';
+});
