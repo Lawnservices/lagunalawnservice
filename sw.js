4b3622adb0aa40fa9faa3652da7d772b93942e03
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-12-12 16:16:09 -0500

    fix

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('mi-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/js/main.js'
+                // Agrega aquí otros recursos que desees cachear
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
