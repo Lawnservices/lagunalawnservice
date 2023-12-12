48cb8e1c071d3736373078971af370a875383f29
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-12-12 17:38:29 -0500

    fiz-web

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js')
-        .then(registration => {
-            console.log('Service Worker registrado con éxito:', registration);
-        })
-        .catch(error => {
-            console.error('Error al registrar el Service Worker:', error);
-        });
+         
 }
