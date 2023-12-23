3e6d4614ce46576873f5e91ab241900ecac92acf
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-12-22 20:49:21 -0500

    fix-web-laguna

--- place.js
+++ place.js
@@ -1,5 +1,3 @@
-document.addEventListener('DOMContentLoaded',function(){
-
   const myPromise = new Promise(function(myResolve, myReject) {
   setTimeout(function(){ myResolve("You want an excellent garden, you are in the right place")},6000);
 });
@@ -8,7 +6,7 @@ myPromise.then(function(value) {
   document.getElementById("place").innerHTML = value;
   document.getElementById("place").style.backgroundColor="#1548";
 });
-})
+ 
 
 
 
