7405dd436839ac980750578017eae9859ee84c43
miguel <80590513+Lawnservices@users.noreply.github.com>
2024-01-01 16:41:20 -0500

    fix

--- js/photos.js
+++ js/photos.js
@@ -2,5 +2,5 @@ function laguna() {
     document.getElementById('nombre').style.color = '#0366d6';
     document.getElementById('calor').style.color = '#04aa6b';
     document.getElementById('calor').innerHTML = 'Call now to make an estimate <br>240-883-8479';
-    document.getElementById('zxcv').innerHTML = 'Subscribete to our Youtube channel';
+    document.getElementById('zxcv').innerHTML = 'Subscribe to our Youtube channel';
 }
