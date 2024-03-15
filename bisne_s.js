d78e03f43ee79c34645042391783b1b3a2821208
miguel <80590513+Lawnservices@users.noreply.github.com>
2024-03-15 14:53:03 -0400

    fix-web

--- bisne_s.js
+++ bisne_s.js
@@ -1,3 +1,16 @@
+const vesa = document.getElementById("navega");
+const vesas = document.getElementById("vermas");
+const cavesa = document.addEventListener("scroll", () =>{
+var scroll_positin = window.scrollY;
+if(scroll_positin > 300){
+navega.style.backgroundColor = "#003349";
+vermas.style.color = "#fff";
+} else{
+  navega.style.backgroundColor = "#299684";
+  vermas.style.color = "#333";
+}
+});
+
 document.getElementById("bisnes").addEventListener("click",function(){
   document.getElementById("bisnes").style.display = "none";
   document.getElementById("domingo").style.display = "block";
