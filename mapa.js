99bff6e7ead9122b2e001affaad14613a9e929fd
miguel <80590513+Lawnservices@users.noreply.github.com>
2024-03-17 12:24:48 -0400

    fix-web

---
+++ mapa.js
@@ -0,0 +1,26 @@
+const cavesa = document.getElementById('navbarr');
+ 
+document.addEventListener('scroll', () => {
+var scroll_position = window.scrollY;
+if(scroll_position > 300){
+  cavesa.style.backgroundColor = '#015462';
+} else{
+   cavesa.style.backgroundColor = '#003349';
+}
+});
+
+function mas(){
+document.getElementById('map').style.color = 'green';
+document.getElementById('cipal').style.color = '#003349';
+};
+setInterval(mas,9000);
+
+document.getElementById('foto1').addEventListener('click',function(){
+document.getElementById('foto1').style.display = 'none';
+document.getElementById('foto2').style.display = 'block';
+});
+
+document.getElementById('foto2').addEventListener('click',function(){
+document.getElementById('foto2').style.display = 'none';
+document.getElementById('foto1').style.display = 'block';
+});
