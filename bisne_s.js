1998eda383b24357a0f6a6929c268df109327814
miguel <80590513+Lawnservices@users.noreply.github.com>
2024-03-17 21:29:40 -0400

    fix

--- bisne_s.js
+++ bisne_s.js
@@ -1,24 +1,29 @@
 const vesa = document.getElementById("navega");
 const vesas = document.getElementById("vermas");
-const cavesa = document.addEventListener("scroll", () =>{
-var scroll_positin = window.scrollY;
-if(scroll_positin > 300){
-navega.style.backgroundColor = "#003349";
-vermas.style.color = "#fff";
-} else{
-  navega.style.backgroundColor = "#299684";
-  vermas.style.color = "#333";
-}
+const cavesa = document.addEventListener("scroll", () => {
+  var scroll_positin = window.scrollY;
+  if (scroll_positin > 300) {
+    navega.style.backgroundColor = "#003349";
+    vermas.style.color = "#fff";
+  } else {
+    navega.style.backgroundColor = "#299684";
+    vermas.style.color = "#333";
+  }
 });
 
-document.getElementById("bisnes").addEventListener("click",function(){
+document.getElementById("bisnes").addEventListener("click", function () {
   document.getElementById("bisnes").style.display = "none";
   document.getElementById("domingo").style.display = "block";
   document.getElementById("bisnes1").style.display = "block";
 });
 
-document.getElementById("bisnes1").addEventListener("click",function(){
+document.getElementById("bisnes1").addEventListener("click", function () {
   document.getElementById("bisnes1").style.display = "none";
   document.getElementById("bisnes").style.display = "block";
-   document.getElementById("domingo").style.display = "none";
+  document.getElementById("domingo").style.display = "none";
 });
+
+// document.addEventListener("click", myFunction);
+// function myFunction() {
+//   document.getElementById("domingo").style.color = "#000";
+// };
