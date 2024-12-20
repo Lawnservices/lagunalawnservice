76dc1c915a56800124486436febc5a78b5f0595a
miguel <80590513+Lawnservices@users.noreply.github.com>
2024-12-20 18:46:26 -0500

    fix

--- navegacion.js
+++ navegacion.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navegac = document.getElementById("nav");
-   
+  const navegacion = document.getElementById("nave");
   const navegacio = document.getElementById("naves");
 
   const enla = document.createElement("a");
-   
+  const enlace = document.createElement("a");
   const enlac = document.createElement("a");
   
   enla.href = "https://lagunalawnservice.com";
@@ -16,6 +16,6 @@ document.addEventListener("DOMContentLoaded", function () {
   enlac.textContent='Prices';
   
   navegac.appendChild(enla);
-   
+  navegacion.appendChild(enlace);
   navegacio.appendChild(enlac);
 });
