093952308ac01196adf82b0da5aa9bfac62e233e
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2025-04-23 12:31:11 -0400

    fix-web

--- time.js
+++ time.js
@@ -1,13 +1,13 @@
 function timesaludo(){
 const  hour = new Date().getHours();
 let greeting;
-
+const mensaje = 'time to put down new mulch.';
 if(hour < 12 ){
-    greeting = 'Good morning';
+    greeting = 'Good morning' +' '+ mensaje;
 } else if(hour < 18) {
-    greeting = 'Good day';
+    greeting = 'Good day' +' '+ mensaje;
 } else{
-    greeting = 'Good evening';
+    greeting = 'Good evening' +' '+ mensaje;
 }
 document.getElementById('time').innerHTML = greeting;
 }
