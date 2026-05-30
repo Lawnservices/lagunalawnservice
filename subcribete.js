89fabbb879626109c229e174311ee6e1d79f65c6
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-05-30 09:56:58 -0400

    fix

---
+++ subcribete.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById("subscribeBtn");
+
+    
+    const lang = navigator.language || navigator.userLanguage;
+
+    dioma
+    if (lang.startsWith("en")) {
+        btn.textContent = "Subscribe to my channel";
+    } else {
+        btn.textContent = "Suscríbete a mi canal";
+    }
+
+   
+    btn.onclick = () => {
+        window.open("https://www.youtube.com/channel/UC8Q51GWY9f5YsIQyLwNMfbw?sub_confirmation=1", "_blank");
+    };
