c9b123a88d49efd9e00e6823a3031962bd37c158
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-08-05 09:05:34 -0400

    fix

--- prices/prices.js
+++ prices/prices.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const masa = document.getElementById("titulos");
-  document.addEventListener("scroll", () => {
-    var scroll_position = window.scrollY;
-    if (scroll_position > 800) {
-      masa.style.display = "block";
-      masa.innerHTML =
-        "<h3>THE BEST PRICES IN THE AREA.</h3>";
-    } else {
-      masa.style.backgroundColor = "#061125";
-    }
-  });
+  // const masa = document.getElementById("titulos");
+  // document.addEventListener("scroll", () => {
+  //   var scroll_position = window.scrollY;
+  //   if (scroll_position > 800) {
+  //     masa.style.display = "block";
+  //     masa.innerHTML =
+  //       "<h3>THE BEST PRICES IN THE AREA.</h3>";
+  //   } else {
+  //     masa.style.backgroundColor = "#061125";
+  //   }
+  // });
 
   let hora = new Date().getHours();
 
